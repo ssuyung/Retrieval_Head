9a691ad99033cda3cf2696ea460fbef56e5ababc
ssuyung <ssuyung@gmail.com>
2024-11-28 07:03:52 +0000

    update print_top_head.py to print 3.2 1B

--- print_top_head.py
+++ print_top_head.py
@@ -1,7 +1,7 @@
 ## load head score file, llama-2-7b-80k for example
 import json
 import numpy as np
-with open('./head_score/llama-2-7b-80k.json') as file:
+with open('./head_score/Llama-3.2-1B-Instruct.json') as file:
     head_list = json.loads(file.readline())
 ## use the average retrieval score and ranking
 head_score_list = [([int(ll) for ll in l[0].split("-")],np.mean(l[1])) for l in head_list.items()]
